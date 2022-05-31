c356791d4da70fbafecaa26e8ecfb820b0c5ea8f
Richard Trotta <trotta@cua.edu>
2022-05-31 14:19:03 -0400

    Added bash script and finished up python script. The simc weights are now dynamically calculated. The data charge needs to be added.

--- run_HeeP_Analysis.sh
+++ run_HeeP_Analysis.sh
@@ -4,12 +4,12 @@ KIN=$1
 
 ANA_DIR="/group/c-kaonlt/USERS/${USER}/simc_gfortran/scripts/"
 
-InDATAFilename='${KIN}_Raw_Data.root'
-InDUMMYFilename='${KIN}_Raw_DummyData.root'
-InSIMCFilename='Heep_Coin_${KIN}.root'
-OutDATAFilename='${KIN}_Analysed_Data'
-OutDUMMYFilename='${KIN}_Analysed_DummyData'
-OutFullAnalysisFilename='${KIN}_FullAnalysis'
+InDATAFilename="${KIN}_Raw_Data.root"
+InDUMMYFilename="${KIN}_Raw_DummyData.root"
+InSIMCFilename="Heep_Coin_${KIN}.root"
+OutDATAFilename="${KIN}_Analysed_Data"
+OutDUMMYFilename="${KIN}_Analysed_DummyData"
+OutFullAnalysisFilename="${KIN}_FullAnalysis"
 
 cd $ANA_DIR
 pwd

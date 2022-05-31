e919c9a46e88721934225ecbe0aef25ce9fa110c
Richard Trotta <trotta@cua.edu>
2022-05-31 14:25:33 -0400

    Added bash script and finished up python script. The simc weights are now dynamically calculated. The data charge needs to be added.

--- run_HeeP_Analysis.sh
+++ run_HeeP_Analysis.sh
@@ -13,7 +13,7 @@ OutFullAnalysisFilename="${KIN}_FullAnalysis"
 
 cd $ANA_DIR
 pwd
-root -l -q "${ANA_DIR}Analysed_COIN.C('$InDATAFilename','$OutDATAFilename')"
-root -l -q "${ANA_DIR}Analysed_COIN.C('$InDUMMYFilename','$OutDUMMYFilename')"
+"root -l -q \"${ANA_DIR}Analysed_COIN.C(${InDATAFilename},${OutDATAFilename})\""
+"root -l -q \"${ANA_DIR}Analysed_COIN.C(${InDUMMYFilename},${OutDUMMYFilename})\""
 
 python3 HeepCoin.py "${OutDATAFilename}.root" "${OutDUMMYFilename}.root" ${InSIMCFilename} ${OutFullAnalysisFilename}

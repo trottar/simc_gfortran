5ba791707ba10862e8e87b1e44aa69a7fa902f31
Richard Trotta <trotta@cua.edu>
2022-05-31 12:09:09 -0400

    fixed typo

--- scripts/Heepcoin.py
+++ scripts/Heepcoin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-05-31 12:07:43 trottar"
+# Time-stamp: "2022-05-31 12:08:52 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -314,20 +314,20 @@ for i in range(nEntries_TBRANCH_SIMC):
 
   # Select the cuts
   #HMS
-  CUT1 = hsdelta >=-8.0 && hsdelta <=8.0
-  CUT2 = hsxptar >=-0.08 && hsxpfp <=0.08
-  CUT3 = hsyptar >=-0.045 && hsypfp <=0.045
+  CUT1 = hsdelta >=-8.0 and hsdelta <=8.0
+  CUT2 = hsxptar >=-0.08 and hsxpfp <=0.08
+  CUT3 = hsyptar >=-0.045 and hsypfp <=0.045
 
   #SHMS    
-  CUT4 = ssdelta >=-10.0 && hsdelta <=20.0
-  CUT5 = ssxptar >=-0.06 && hsxpfp <=0.06
-  CUT6 = hsyptar >=-0.04 && hsypfp <=0.04
+  CUT4 = ssdelta >=-10.0 and hsdelta <=20.0
+  CUT5 = ssxptar >=-0.06 and hsxpfp <=0.06
+  CUT6 = hsyptar >=-0.04 and hsypfp <=0.04
 
   #........................................
 
   #Fill SIMC events
 
-  if(CUT1 && CUT2 && CUT3 && CUT4 && CUT5 && CUT6)
+  if(CUT1 and CUT2 and CUT3 and CUT4 and CUT5 and CUT6)
     
       H_ssxfp_SIMC.Fill(ssxfp, Weight)
       H_ssyfp_SIMC.Fill(ssyfp, Weight)
@@ -358,18 +358,18 @@ for i in range(nEntries_TBRANCH_DATA):
   TBRANCH_DATA.GetEntry(i)
 
   #CUTs Definations 
-  SHMS_FixCut = P_hod_goodstarttime_data == 1&&P_dc_InsideDipoleExit_data == 1 # && P_hod_betanotrack_data > 0.5 && P_hod_betanotrack_data < 1.4
-  SHMS_Acceptance = P_gtr_dp_data>=-10.0 && P_gtr_dp_data<=20.0&&P_gtr_xptar_data>=-0.06&&P_gtr_xptar_data<=0.06&&P_gtr_yptar_data>=-0.04&&P_gtr_yptar_data<=0.04
-  SHMS_ELECTRON_PID = P_cal_etottracknorm_data >= 0.85 && P_cal_etottracknorm_data <= 1.2 # P_hgcer_npeSum_data >=0.5 && P_aero_npeSum_data >=0.5
+  SHMS_FixCut = P_hod_goodstarttime_data == 1andP_dc_InsideDipoleExit_data == 1 # and P_hod_betanotrack_data > 0.5 and P_hod_betanotrack_data < 1.4
+  SHMS_Acceptance = P_gtr_dp_data>=-10.0 and P_gtr_dp_data<=20.0andP_gtr_xptar_data>=-0.06andP_gtr_xptar_data<=0.06andP_gtr_yptar_data>=-0.04andP_gtr_yptar_data<=0.04
+  SHMS_ELECTRON_PID = P_cal_etottracknorm_data >= 0.85 and P_cal_etottracknorm_data <= 1.2 # P_hgcer_npeSum_data >=0.5 and P_aero_npeSum_data >=0.5
 
-  HMS_FixCut = H_hod_goodscinhit_data == 1 && H_hod_goodstarttime_data == 1&&H_dc_InsideDipoleExit_data == 1
-  HMS_Acceptance = H_gtr_dp_data>=-8.0 && H_gtr_dp_data<=8.0&&H_gtr_xptar_data>=-0.08&&H_gtr_xptar_data<=0.08&&H_gtr_yptar_data>=-0.045&&H_gtr_yptar_data<=0.045       
-  HMS_ELECTRON_PID = H_cer_npeSum_data >=0.5 && H_cal_etotnorm_data >=0.8 && H_cal_etotnorm_data <=1.2
+  HMS_FixCut = H_hod_goodscinhit_data == 1 and H_hod_goodstarttime_data == 1andH_dc_InsideDipoleExit_data == 1
+  HMS_Acceptance = H_gtr_dp_data>=-8.0 and H_gtr_dp_data<=8.0andH_gtr_xptar_data>=-0.08andH_gtr_xptar_data<=0.08andH_gtr_yptar_data>=-0.045andH_gtr_yptar_data<=0.045       
+  HMS_ELECTRON_PID = H_cer_npeSum_data >=0.5 and H_cal_etotnorm_data >=0.8 and H_cal_etotnorm_data <=1.2
 
   #........................................
 
-  #if(SHMS_FixCut && SHMS_Acceptance && SHMS_ELECTRON_PID) 
-  if(SHMS_FixCut && SHMS_Acceptance) 
+  #if(SHMS_FixCut and SHMS_Acceptance and SHMS_ELECTRON_PID) 
+  if(SHMS_FixCut and SHMS_Acceptance) 
     
       H_ssxfp_DATA.Fill(ssxfp_data)
       H_ssyfp_DATA.Fill(ssyfp_data)
@@ -381,8 +381,8 @@ for i in range(nEntries_TBRANCH_DATA):
 
     
 
-  #if(HMS_FixCut && HMS_Acceptance && HMS_ELECTRON_PID)
-  if(HMS_FixCut && HMS_Acceptance)
+  #if(HMS_FixCut and HMS_Acceptance and HMS_ELECTRON_PID)
+  if(HMS_FixCut and HMS_Acceptance)
     
       H_pmiss_DATA.Fill(pm_data)	
       H_emiss_DATA.Fill(em_data)	
@@ -410,18 +410,18 @@ for i in range(nEntries_TBRANCH_DUMMY):
   #......... Define Cuts.................
 
   #CUTs Definations 
-  SHMS_FixCut = P_hod_goodstarttime_dummy == 1&&P_dc_InsideDipoleExit_dummy == 1 # && P_hod_betanotrack_dummy > 0.5 && P_hod_betanotrack_dummy < 1.4
-  SHMS_Acceptance = P_gtr_dp_dummy>=-10.0 && P_gtr_dp_dummy<=20.0&&P_gtr_xptar_dummy>=-0.06&&P_gtr_xptar_dummy<=0.06&&P_gtr_yptar_dummy>=-0.04&&P_gtr_yptar_dummy<=0.04
-  SHMS_ELECTRON_PID = P_cal_etottracknorm_dummy >= 0.85 && P_cal_etottracknorm_dummy <= 1.2 # P_hgcer_npeSum_dummy >=0.5 && P_aero_npeSum_dummy >=0.5
+  SHMS_FixCut = P_hod_goodstarttime_dummy == 1andP_dc_InsideDipoleExit_dummy == 1 # and P_hod_betanotrack_dummy > 0.5 and P_hod_betanotrack_dummy < 1.4
+  SHMS_Acceptance = P_gtr_dp_dummy>=-10.0 and P_gtr_dp_dummy<=20.0andP_gtr_xptar_dummy>=-0.06andP_gtr_xptar_dummy<=0.06andP_gtr_yptar_dummy>=-0.04andP_gtr_yptar_dummy<=0.04
+  SHMS_ELECTRON_PID = P_cal_etottracknorm_dummy >= 0.85 and P_cal_etottracknorm_dummy <= 1.2 # P_hgcer_npeSum_dummy >=0.5 and P_aero_npeSum_dummy >=0.5
 
-  HMS_FixCut = H_hod_goodscinhit_dummy == 1 && H_hod_goodstarttime_dummy == 1&&H_dc_InsideDipoleExit_dummy == 1
-  HMS_Acceptance = H_gtr_dp_dummy>=-8.0 && H_gtr_dp_dummy<=8.0&&H_gtr_xptar_dummy>=-0.08&&H_gtr_xptar_dummy<=0.08&&H_gtr_yptar_dummy>=-0.045&&H_gtr_yptar_dummy<=0.045       
-  HMS_ELECTRON_PID = H_cer_npeSum_dummy >=0.5 && H_cal_etotnorm_dummy >=0.8 && H_cal_etotnorm_dummy <=1.2
+  HMS_FixCut = H_hod_goodscinhit_dummy == 1 and H_hod_goodstarttime_dummy == 1andH_dc_InsideDipoleExit_dummy == 1
+  HMS_Acceptance = H_gtr_dp_dummy>=-8.0 and H_gtr_dp_dummy<=8.0andH_gtr_xptar_dummy>=-0.08andH_gtr_xptar_dummy<=0.08andH_gtr_yptar_dummy>=-0.045andH_gtr_yptar_dummy<=0.045       
+  HMS_ELECTRON_PID = H_cer_npeSum_dummy >=0.5 and H_cal_etotnorm_dummy >=0.8 and H_cal_etotnorm_dummy <=1.2
 
   #........................................
 
-  #if(SHMS_FixCut && SHMS_Acceptance && SHMS_ELECTRON_PID) 
-  if(SHMS_FixCut && SHMS_Acceptance) 
+  #if(SHMS_FixCut and SHMS_Acceptance and SHMS_ELECTRON_PID) 
+  if(SHMS_FixCut and SHMS_Acceptance) 
     
       H_ssxfp_DUMMY.Fill(ssxfp_dummy)
       H_ssyfp_DUMMY.Fill(ssyfp_dummy)
@@ -433,8 +433,8 @@ for i in range(nEntries_TBRANCH_DUMMY):
 
     
 
-  #if(HMS_FixCut && HMS_Acceptance && HMS_ELECTRON_PID)
-  if(HMS_FixCut && HMS_Acceptance)
+  #if(HMS_FixCut and HMS_Acceptance and HMS_ELECTRON_PID)
+  if(HMS_FixCut and HMS_Acceptance)
     
       H_pmiss_DUMMY.Fill(pm_dummy)	
       H_emiss_DUMMY.Fill(em_dummy)	

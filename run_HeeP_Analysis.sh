1dc5be46e2de8f3a97ca3a421ba31dc93949cbb5
Richard Trotta <trotta@cua.edu>
2022-05-31 14:45:56 -0400

    Added bash script and finished up python script. The simc weights are now dynamically calculated. The data charge needs to be added.

--- run_HeeP_Analysis.sh
+++ run_HeeP_Analysis.sh
@@ -4,16 +4,16 @@ KIN=$1
 
 ANA_DIR="/group/c-kaonlt/USERS/${USER}/simc_gfortran/scripts"
 
-InDATAFilename="Raw_Data_$KIN.root"
-InDUMMYFilename="Raw_DummyData_$KIN.root"
-InSIMCFilename="Heep_Coin_$KIN.root"
-OutDATAFilename="Analysed_Data_$KIN"
-OutDUMMYFilename="Analysed_DummyData_$KIN"
-OutFullAnalysisFilename="FullAnalysis_$KIN"
+InDATAFilename="Raw_Data_${KIN}.root"
+InDUMMYFilename="Raw_DummyData_${KIN}.root"
+InSIMCFilename="Heep_Coin_${KIN}.root"
+OutDATAFilename="Analysed_Data_${KIN}"
+OutDUMMYFilename="Analysed_DummyData_${KIN}"
+OutFullAnalysisFilename="FullAnalysis_${KIN}"
 
 cd $ANA_DIR
 pwd
-root -l -q "$ANA_DIR/Analysed_COIN.C($InDATAFilename,$OutDATAFilename)"
-root -l -q "$ANA_DIR/Analysed_COIN.C($InDUMMYFilename,$OutDUMMYFilename)"
+eval "hcana -l -q \"$ANA_DIR/Analysed_COIN.C($InDATAFilename,$OutDATAFilename)\""
+eval "hcana -l -q \"$ANA_DIR/Analysed_COIN.C($InDUMMYFilename,$OutDUMMYFilename)\""
 
 python3 HeepCoin.py "${OutDATAFilename}.root" "${OutDUMMYFilename}.root" ${InSIMCFilename} ${OutFullAnalysisFilename}

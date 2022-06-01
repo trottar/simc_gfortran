5fda7ba707f7bb367493fc5275c5f8edc7364fd9
Richard Trotta <trotta@cua.edu>
2022-06-01 01:13:40 -0400

    Have the dynamic charge part working. Need to add flags for flare

--- run_HeeP_Analysis.sh
+++ run_HeeP_Analysis.sh
@@ -1,6 +1,24 @@
 #! /bin/bash
 
-KIN=$1
+while getopts 'ha' flag; do
+    case "${flag}" in
+        h) 
+        echo "---------------------------------------------------"
+        echo "./run_HeeP_Analysis.sh -{flags} {kinematic setting}"
+        echo "---------------------------------------------------"
+        echo
+        echo "The following flags can be called for the heep analysis..."
+        echo "    -h, help"
+        echo "    -a, analyze"
+        exit 0
+        ;;
+        a) a_flag='true' ;;
+        *) print_usage
+        exit 1 ;;
+    esac
+done
+
+KIN=$2
 
 ANA_DIR="/group/c-kaonlt/USERS/${USER}/simc_gfortran/scripts"
 
@@ -12,18 +30,21 @@ OutDUMMYFilename="Analysed_DummyData_$KIN"
 OutFullAnalysisFilename="FullAnalysis_$KIN"
 
 cd $ANA_DIR
-echo
-echo "Analysing data..."
-echo
-root -l <<EOF 
-.x $ANA_DIR/Analysed_COIN.C("$InDATAFilename","$OutDATAFilename")
-EOF
-echo
-echo "Analysing dummy data..."
-echo
-root -l <<EOF 
-.x $ANA_DIR/Analysed_COIN.C("$InDUMMYFilename","$OutDUMMYFilename")
-EOF
+
+if [[ $g_flag = "true" ]]; then
+    echo
+    echo "Analysing data..."
+    echo
+    root -l <<EOF 
+    .x $ANA_DIR/Analysed_COIN.C("$InDATAFilename","$OutDATAFilename")
+    EOF
+    echo
+    echo "Analysing dummy data..."
+    echo
+    root -l <<EOF 
+    .x $ANA_DIR/Analysed_COIN.C("$InDUMMYFilename","$OutDUMMYFilename")
+    EOF
+fi
 
 python3 HeepCoin.py "${OutDATAFilename}.root" "${OutDUMMYFilename}.root" ${InSIMCFilename} ${OutFullAnalysisFilename}
 

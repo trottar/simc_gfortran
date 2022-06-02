6186e229d8c663ce0bf27b3a661206b350a3e8fa
Richard Trotta <trotta@cua.edu>
2022-06-01 21:11:51 -0400

    Added dynamic selection of elastic kinematics based off theta_e of interest.

--- scripts/elas_kin.py
+++ scripts/elas_kin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-06-01 20:23:43 trottar"
+# Time-stamp: "2022-06-01 21:11:03 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -15,24 +15,29 @@ from numpy import f2py
 import pandas as pd
 import os,sys
 
-inp_f = 'elas_kin'
+##################################################################################################################################################
+# Fortran compiler to extract elastic kinematic data
 
-with open("%s.f" % inp_f) as srcf:
+with open("elas_kin.f") as srcf:
     srcc = srcf.read()
-f2py.compile(srcc,modulename='pyadd', verbose=False)
-#f2py.compile(srcc,modulename='pyadd', verbose=True)
+f2py.compile(srcc,modulename='elas_kin', verbose=False)
 
 try:
-    import pyadd
+    import elas_kin
 except ModuleNotFoundError:
     print("ERROR: Module for %s not found..." % inp_f)
     sys.exit(0)
 
-print("\n\nDoc for %s..." % inp_f)    
-print(pyadd.__doc__)
+print("\n\nDoc for elas_kin.f...")    
+print(elas_kin.__doc__)
+
+##################################################################################################################################################
+
+beam_energy = float(sys.argv[1])
+theta_e = float(sys.argv[2])
+theta_lim = 0.05
 
 N = 1000
-beam_energy = 10.600
 Th_e = np.zeros(N)
 Q2 = np.zeros(N)
 E_e = np.zeros(N)
@@ -42,14 +47,14 @@ Mott = np.zeros(N)
 Sig_p = np.zeros(N)
 
 # Th_e,Q2,E_e,Th_p,P_p,Mott,Sig_p are will get redefined with the proper values from calculations
-pyadd.kin_table(beam_energy,Th_e,Q2,E_e,Th_p,P_p,Mott,Sig_p,N)
+elas_kin.kin_table(beam_energy,Th_e,Q2,E_e,Th_p,P_p,Mott,Sig_p,N)
 
 kinDict = {
 
     'Theta_e' : Th_e,
     'Q2' : Q2,
     'E_e' : E_e,
-    'Th_p' : Th_p,
+    'Theta_p' : Th_p,
     'P_p' : P_p,
     'Mott' : Mott,
     'Sig_p (fm^2/sr)' : Sig_p,
@@ -58,7 +63,9 @@ kinDict = {
 kin_data = {i : kinDict[i] for i in sorted(kinDict.keys())}
 
 # Convert merged dictionary to a pandas dataframe then sort it
-table  = pd.DataFrame([kin_data], columns=kin_data.keys())
+table  = pd.DataFrame(kin_data, columns=kin_data.keys())
 table = table.reindex(sorted(table.columns), axis=1)
 
-print(table['P_p'])
+good_kin = table[table['Theta_e'].between(theta_e-theta_lim,theta_e+theta_lim,inclusive=True)]
+
+print(good_kin)

c72ff87152694a6e2fa14fe7f9b7092b933e47aa
Richard Trotta <trotta@cua.edu>
2022-06-02 16:08:15 -0400

    Testing new script for cointime cuts

--- scripts/Analysed_COIN.py
+++ scripts/Analysed_COIN.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-06-02 16:07:26 trottar"
+# Time-stamp: "2022-06-02 16:08:10 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -65,7 +65,7 @@ OUTPATH = proc_root[2] # Get pathing for OUTPATH
 
 #################################################################################################################################################################
 UTILPATH = lt.SetPath(os.path.realpath(__file__)).getPath("UTILPATH")
-rootName = "%s/ROOTfiles/Analysis/%s/%s_%s_%s.root" % (UTILPATH, runType, ROOTPrefix, runNum, MaxEvent)     # Input file location and variables taking
+rootName = "HeeP/ROOTfiles/Analysis/%s/%s_%s_%s.root" % (UTILPATH, ROOTPrefix, runNum, MaxEvent)     # Input file location and variables taking
 s_tree = up.open(rootName)["TSP"]
 
 P_BCM4A_scalerCharge = s_tree.array("P.BCM4A.scalerCharge")

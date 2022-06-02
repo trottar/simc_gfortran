d184c02db8aec95223327105d42e43303e217a5e
Richard Trotta <trotta@cua.edu>
2022-06-02 13:13:47 -0400

    Random subtraction added. Data is much smaller than simc though. Need to further investigate

--- scripts/HeepCoin.py
+++ scripts/HeepCoin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-06-02 13:11:13 trottar"
+# Time-stamp: "2022-06-02 13:12:24 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -167,7 +167,7 @@ TSCALER_DUMMY  = up.open(rootFile_DUMMY)["scaler"]
 
 ################################################################################################################################################
 # Charge calculation
-thres_curr = 40
+thres_curr = 2.5
 NBCM = 5
 
 # Data charge calculation

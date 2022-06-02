57249ad929f0680b2665c69dca457e31cd8497c0
Richard Trotta <trotta@cua.edu>
2022-06-02 11:57:12 -0400

    Added timing cuts for random subtractions. In the testing phase

--- scripts/HeepCoin.py
+++ scripts/HeepCoin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-06-02 11:55:48 trottar"
+# Time-stamp: "2022-06-02 11:57:07 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -581,7 +581,7 @@ H_W_DATA.Add(H_W_DATA.Scale(1.0/nWindows),-1)
 '''
 
 # Dummy Random subtraction
-'''
+#'''
 H_ssxfp_DUMMY.Add(H_ssxfp_DUMMY.Scale(1.0/nWindows),-1)
 H_ssyfp_DUMMY.Add(H_ssyfp_DUMMY.Scale(1.0/nWindows),-1)
 H_ssxpfp_DUMMY.Add(H_ssxpfp_DUMMY.Scale(1.0/nWindows),-1)
@@ -605,7 +605,7 @@ H_pmx_DUMMY.Add(H_pmx_DUMMY.Scale(1.0/nWindows),-1)
 H_pmy_DUMMY.Add(H_pmy_DUMMY.Scale(1.0/nWindows),-1)
 H_pmz_DUMMY.Add(H_pmz_DUMMY.Scale(1.0/nWindows),-1)
 H_W_DUMMY.Add(H_W_DUMMY.Scale(1.0/nWindows),-1)
-'''
+
 
 # Dummy Subtraction
 H_ssxfp_DATA.Add(H_ssxfp_DUMMY,-1)

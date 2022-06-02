79992c0a76070a6cae7e7517ab39f196223be32e
Richard Trotta <trotta@cua.edu>
2022-06-02 16:31:51 -0400

    Testing new script for cointime cuts

--- scripts/HeepCoin.py
+++ scripts/HeepCoin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-06-02 14:22:31 trottar"
+# Time-stamp: "2022-06-02 16:31:30 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -155,9 +155,11 @@ InFile_DATA = ROOT.TFile.Open(rootFile, "OPEN")
 InFile_DUMMY = ROOT.TFile.Open(rootFile_DUMMY, "OPEN")
 InFile_SIMC = ROOT.TFile.Open(rootFile_SIMC, "READ")
 
-TBRANCH_DATA  = InFile_DATA.Get("hist")
+#TBRANCH_DATA  = InFile_DATA.Get("hist")
+TBRANCH_DATA  = InFile_DATA.Get("Cut_Proton_Events_All")
 nEntries_TBRANCH_DATA  = TBRANCH_DATA.GetEntries()
-TBRANCH_DUMMY  = InFile_DUMMY.Get("hist")
+#TBRANCH_DUMMY  = InFile_DUMMY.Get("hist")
+TBRANCH_DUMMY  = InFile_DUMMY.Get("Cut_Proton_Events_All")
 nEntries_TBRANCH_DUMMY  = TBRANCH_DUMMY.GetEntries()
 TBRANCH_SIMC  = InFile_SIMC.Get("h10")
 nEntries_TBRANCH_SIMC  = TBRANCH_SIMC.GetEntries()

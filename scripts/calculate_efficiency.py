86214c0598791baff76c4a7fb2f2ffcc20bcd520
Richard Trotta <trotta@cua.edu>
2022-06-13 09:28:58 -0400

    Testing efficiency script

--- scripts/calculate_efficiency.py
+++ scripts/calculate_efficiency.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-06-13 09:27:30 trottar"
+# Time-stamp: "2022-06-13 09:28:53 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -55,9 +55,7 @@ hElTrack = eff_data["HMS_Elec_SING_TRACK_EFF"].iloc[0]
 hElCer = eff_data["HMS_Cer_SING_Elec_Eff"].iloc[0]
 hHodo3_4 = eff_data["HMS_Hodo_3_of_4_EFF"].iloc[0]
 
-
-print(eff_data["Non_Scaler_EDTM_Live_Time"].iloc[0],eff_data["SHMS_Pion_SING_TRACK_EFF"].iloc[0],eff_data["SHMS_Aero_SING_Pion_Eff"].iloc[0],eff_data["SHMS_Hodo_3_of_4_EFF"].iloc[0],eff_data["HMS_Elec_SING_TRACK_EFF"].iloc[0],eff_data["HMS_Cer_SING_Elec_Eff"].iloc[0],eff_data["HMS_Hodo_3_of_4_EFF"].iloc[0])
-
+print("EDTM",EDTM,"pPiTrack",pPiTrack,"pPiAero",pPiAero,"pHodo3",pHodo3,"hElTrack",hElTrack,"hElCer",hElCer,"hHodo3",hHodo3)
 data_efficiency = EDTM*pHodo3_4*hHodo3_4
 
 print(data_efficiency)

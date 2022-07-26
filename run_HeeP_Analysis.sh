8884258b9dafe2996e4e5f020d9b43d27c4ae43f
Richard Trotta <trotta@cua.edu>
2022-07-26 16:57:02 -0400

    Testing analysis heep sing

--- run_HeeP_Analysis.sh
+++ run_HeeP_Analysis.sh
@@ -1,5 +1,30 @@
 #! /bin/bash
 
+# Runs script in the ltsep python package that grabs current path enviroment
+if [[ ${HOSTNAME} = *"cdaq"* ]]; then
+    PATHFILE_INFO=`python3 /home/cdaq/pionLT-2021/hallc_replay_lt/UTIL_PION/bin/python/ltsep/scripts/getPathDict.py $PWD` # The output of this python script is just a comma separated string
+elif [[ ${HOSTNAME} = *"farm"* ]]; then
+    PATHFILE_INFO=`python3 /u/home/${USER}/.local/lib/python3.4/site-packages/ltsep/scripts/getPathDict.py $PWD` # The output of this python script is just a comma separated string
+fi
+
+# Split the string we get to individual variables, easier for printing and use later
+VOLATILEPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f1` # Cut the string on , delimitter, select field (f) 1, set variable to output of command
+ANALYSISPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f2`
+HCANAPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f3`
+REPLAYPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f4`
+UTILPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f5`
+PACKAGEPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f6`
+OUTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f7`
+ROOTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f8`
+REPORTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f9`
+CUTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f10`
+PARAMPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f11`
+SCRIPTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f12`
+ANATYPE=`echo ${PATHFILE_INFO} | cut -d ','  -f13`
+USER=`echo ${PATHFILE_INFO} | cut -d ','  -f14`
+HOST=`echo ${PATHFILE_INFO} | cut -d ','  -f15`
+SIMCPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f16`
+
 while getopts 'haos' flag; do
     case "${flag}" in
         h) 
@@ -26,34 +51,13 @@ done
 
 if [[ $a_flag = "true" || $o_flag = "true" ]]; then
     KIN=$2
+elif [[ $s_flag = "true" ]]; then
+    spec=$2
+    SPEC=$(echo "$spec" | tr '[:lower:]' '[:upper:]')
+    KIN=$3
 else
     KIN=$1
 fi
-
-# Runs script in the ltsep python package that grabs current path enviroment
-if [[ ${HOSTNAME} = *"cdaq"* ]]; then
-    PATHFILE_INFO=`python3 /home/cdaq/pionLT-2021/hallc_replay_lt/UTIL_PION/bin/python/ltsep/scripts/getPathDict.py $PWD` # The output of this python script is just a comma separated string
-elif [[ ${HOSTNAME} = *"farm"* ]]; then
-    PATHFILE_INFO=`python3 /u/home/${USER}/.local/lib/python3.4/site-packages/ltsep/scripts/getPathDict.py $PWD` # The output of this python script is just a comma separated string
-fi
-
-# Split the string we get to individual variables, easier for printing and use later
-VOLATILEPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f1` # Cut the string on , delimitter, select field (f) 1, set variable to output of command
-ANALYSISPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f2`
-HCANAPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f3`
-REPLAYPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f4`
-UTILPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f5`
-PACKAGEPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f6`
-OUTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f7`
-ROOTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f8`
-REPORTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f9`
-CUTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f10`
-PARAMPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f11`
-SCRIPTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f12`
-ANATYPE=`echo ${PATHFILE_INFO} | cut -d ','  -f13`
-USER=`echo ${PATHFILE_INFO} | cut -d ','  -f14`
-HOST=`echo ${PATHFILE_INFO} | cut -d ','  -f15`
-SIMCPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f16`
 
 if [[ $s_flag = "true" ]]; then
     InDATAFilename="Raw_Data_${SPEC}_${KIN}.root"

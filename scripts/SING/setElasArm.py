b4b0a0fa16e1110314e55bf6ae4cc13bf9491f09
Richard Trotta <trotta@cua.edu>
2022-07-26 14:23:29 -0400

    Testing singles scripts

--- scripts/SING/setElasArm.py
+++ scripts/SING/setElasArm.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-07-26 14:22:45 trottar"
+# Time-stamp: "2022-07-26 14:23:21 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -12,14 +12,13 @@
 #
 import sys,os,math
 
-'''
 KIN = sys.argv[1]
 SPEC = sys.argv[2]
 ebeam_elas = sys.argv[3]
 eTh_elas = sys.argv[4]
 InputSIMC = sys.argv[5]
 inp_table = sys.argv[6]
-'''
+
 ################################################################################################################################################
 '''
 ltsep package import and pathing definitions
@@ -34,8 +33,6 @@ UTILPATH=lt.UTILPATH
 SIMCPATH=lt.SIMCPATH
 
 ################################################################################################################################################
-
-InputSIMC = "Heep_%s_%s" % (SPEC,KIN)
 
 # Open inp_f file to grab prescale values and tracking efficiency
 inp_f = SIMCPATH+"/input/%s.inp" % InputSIMC

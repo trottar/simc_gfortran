d2647551ddb939565dfc8534d8dc7d85324f41d7
Richard Trotta <trotta@cua.edu>
2022-07-26 11:35:53 -0400

    Testing elas

--- scripts/SING/setElasArm.py
+++ scripts/SING/setElasArm.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-07-26 11:35:07 trottar"
+# Time-stamp: "2022-07-26 11:35:49 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -33,8 +33,9 @@ print(inp_table)
 data_str = inp_table.split("Sig_p (fm^2/sr)")[1]
 data_lst = data_str.split("\n")
 for i,l in enumerate(data_lst):
-    data_lst[i] = l[0].split('     ')
-print(data_lst)
+    print(l)
+    data_lst[i] = l.split('     ')
+#print(data_lst)
 
     
 #for line in inp_table:

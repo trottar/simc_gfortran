5c47235cbc3b5bc928de7d18ac92123e8b621fee
Richard Trotta <trotta@cua.edu>
2022-07-26 17:03:22 -0400

    Testing analysis heep sing

--- scripts/SING/Analysed_SING.py
+++ scripts/SING/Analysed_SING.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-07-26 09:41:14 trottar"
+# Time-stamp: "2022-07-26 17:02:57 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -28,8 +28,8 @@ import sys, math, os, subprocess
 ##################################################################################################################################################
 
 # Check the number of arguments provided to the script
-if len(sys.argv)-1!=1:
-    print("!!!!! ERROR !!!!!\n Expected 3 arguments\n Usage is with - ROOTfilePrefix RunNumber MaxEvents \n!!!!! ERROR !!!!!")
+if len(sys.argv)-1!=2:
+    print("!!!!! ERROR !!!!!\n Expected 2 arguments\n Usage is with - ROOTfilePrefix RunNumber MaxEvents \n!!!!! ERROR !!!!!")
     sys.exit(1)
 
 ##################################################################################################################################################

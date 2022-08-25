89d3347d203820f7de111c48be6b47d0915380e9
Richard Trotta <trotta@cua.edu>
2022-08-25 00:53:08 -0400

    Removed windows for random subtraction, well commented it all out for coin and sing

--- scripts/COIN/HeepCoin.py
+++ scripts/COIN/HeepCoin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-08-25 00:51:50 trottar"
+# Time-stamp: "2022-08-25 00:53:04 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -54,15 +54,16 @@ OutFilename = sys.argv[11]
 try:
     data_efficiency = sum([float(i) for i in InData_efficiency.split(" ")])/len([float(i) for i in InData_efficiency.split(" ")])
     print("\n\ndata_efficiency=",data_efficiency)
-    G_data_eff = ROOT.TGraph(len(InData_efficiency), np.array(InData_efficiency, 'd'), np.array(data_runNums,'d'))
 
     dummy_efficiency = sum([float(i) for i in InDummy_efficiency.split(" ")])/len([float(i) for i in InDummy_efficiency.split(" ")])
     print("dummy_efficiency=",dummy_efficiency)
-    G_data_eff = ROOT.TGraph(len(InData_efficiency), np.array(InData_efficiency), np.array(data_runNums))
     
 except ValueError:
     print("\nError: Invalid efficiency value found...")
     sys.exit(1)
+
+G_data_eff = ROOT.TGraph(len(InData_efficiency), np.array(InData_efficiency, 'd'), np.array(data_runNums,'d'))    
+G_data_eff = ROOT.TGraph(len(InData_efficiency), np.array(InData_efficiency), np.array(data_runNums))
 
 ###############################################################################################################################################
 ROOT.gROOT.SetBatch(ROOT.kTRUE) # Set ROOT to batch mode explicitly, does not splash anything to screen

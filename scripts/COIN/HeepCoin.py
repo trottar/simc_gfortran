a45296507896ba863e213038e8a57f90ea735cb2
Richard Trotta <trotta@cua.edu>
2022-08-25 01:58:39 -0400

    Removed windows for random subtraction, well commented it all out for coin and sing

--- scripts/COIN/HeepCoin.py
+++ scripts/COIN/HeepCoin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-08-25 01:56:09 trottar"
+# Time-stamp: "2022-08-25 01:58:36 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -829,8 +829,8 @@ G_dummy_eff.SetMarkerSize(1)
 
 G_dummy_eff.SetMarkerColor(kRed)
 
-G_data_eff.Draw()
-G_dummy_eff.Draw("same")
+G_data_eff.Draw("AP")
+G_dummy_eff.Draw("AP")
 
 G_data_eff.SetMinimum(0.7)
 G_dummy_eff.SetMinimum(0.7)

739b64e58ce05ce0f2b0c51c9aa6a4d7fec0def6
Richard Trotta <trotta@cua.edu>
2022-09-26 13:38:58 -0400

    Testing error bars to histograms

--- scripts/COIN/HeepCoin.py
+++ scripts/COIN/HeepCoin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-09-26 13:37:41 trottar"
+# Time-stamp: "2022-09-26 13:38:54 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -64,8 +64,8 @@ print("\n\n",tot_effError,"\n\n")
 
 ###############################################################################################################################################
 # Define total efficiency vs run number plots
-G_data_eff = ROOT.TGraphErrors(len(InData_efficiency.split(' ')), np.array([float(x) for x in data_runNums.split(' ')]),np.array([float(x) for x in InData_efficiency.split(' ')]),np.array([]*len(tot_effError)),np.array(tot_effError))
-G_dummy_eff = ROOT.TGraphErrors(len(InDummy_efficiency.split(' ')), np.array([float(x) for x in dummy_runNums.split(' ')]),np.array([float(x) for x in InDummy_efficiency.split(' ')]),np.array([]*len(tot_effError)),np.array(tot_effError))
+G_data_eff = ROOT.TGraphErrors(len(InData_efficiency.split(' ')), np.array([float(x) for x in data_runNums.split(' ')]),np.array([float(x) for x in InData_efficiency.split(' ')]),np.array([0]*len(tot_effError)),np.array(tot_effError))
+G_dummy_eff = ROOT.TGraphErrors(len(InDummy_efficiency.split(' ')), np.array([float(x) for x in dummy_runNums.split(' ')]),np.array([float(x) for x in InDummy_efficiency.split(' ')]),np.array([0]*len(tot_effError)),np.array(tot_effError))
 
 ###############################################################################################################################################
 ROOT.gROOT.SetBatch(ROOT.kTRUE) # Set ROOT to batch mode explicitly, does not splash anything to screen

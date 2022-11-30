b4ab6062288380887fb742ad7313864b8be7870e
Richard Trotta <trotta@cua.edu>
2022-11-30 03:25:10 -0500

    Testing tbin script, phase 2

--- scripts/Prod/binning/find_tBinRange.py
+++ scripts/Prod/binning/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-11-30 03:23:10 trottar"
+# Time-stamp: "2022-11-30 03:25:08 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -127,25 +127,25 @@ def find_tbins():
         # Progress bar
         Misc.progressBar(i, TBRANCH_RIGHT_DATA.GetEntries())
         H_t_Right.append(-evt.MandelT)
-    rn,rbins = np.histogram(H_t_Right,bins=200)
+    H_t_Right = np.histogram(H_t_Right,bins=200)
 
     H_t_Left = []
-    print("Creating left t-bin histogram...")
+    print("\nCreating left t-bin histogram...")
     # Grab t bin range for EvtsPerBinRange evts
     for i,evt in enumerate(TBRANCH_LEFT_DATA):
         # Progress bar
         Misc.progressBar(i, TBRANCH_LEFT_DATA.GetEntries())
         H_t_Left.append(-evt.MandelT)
-    ln,lbins = np.histogram(H_t_Left,bins=200)
+    H_t_Left = np.histogram(H_t_Left,bins=200)
 
     H_t_Center = []
-    print("Creating center t-bin histogram...")
+    print("\nCreating center t-bin histogram...")
     # Grab t bin range for EvtsPerBinRange evts
     for i,evt in enumerate(TBRANCH_CENTER_DATA):
         # Progress bar
         Misc.progressBar(i, TBRANCH_CENTER_DATA.GetEntries())
         H_t_Center.append(-evt.MandelT)
-    cn,cbins = np.histogram(H_t_Center,bins=200)
+    H_t_Center = np.histogram(H_t_Center,bins=200)
             
     for r,l,c in zip(H_t_Right,H_t_Left,H_t_Center):
 

0f2ee0ad5de8515f7c3ac5a54dd44d5dad8b0168
Richard Trotta <trotta@cua.edu>
2022-11-30 00:56:10 -0500

    Testing tbin script, phase 2

--- scripts/Prod/binning/find_tBinRange.py
+++ scripts/Prod/binning/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-11-30 00:54:27 trottar"
+# Time-stamp: "2022-11-30 00:56:06 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -56,6 +56,9 @@ ltsep package import and pathing definitions
 
 # Import package for cuts
 from ltsep import Root
+from ltsep import Misc
+
+msc=Misc()
 
 lt=Root(os.path.realpath(__file__),"Plot_Prod")
 
@@ -176,7 +179,7 @@ def defineHists(phi_setting):
 
     # Grab t bin range for EvtsPerBinRange evts
     for i,evt in enumerate(TBRANCH_DATA):
-        lt.progressBar(i, TBRANCH_DATA.GetEntries(), bar_length=50)
+        msc.progressBar(i, TBRANCH_DATA.GetEntries(), bar_length=50)
         H_t_BinTest.Fill(-evt.MandelT)
         tbinval = np.array(H_t_BinTest).sum()
         for val,binval in zip(np.linspace(0,0.5,201),range(1,len(np.array(H_t_BinTest)))):

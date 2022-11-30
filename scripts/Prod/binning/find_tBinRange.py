999b339d511d923bdc4cdf9c8c270784433d5fb6
Richard Trotta <trotta@cua.edu>
2022-11-30 03:28:18 -0500

    Testing tbin script, phase 2

--- scripts/Prod/binning/find_tBinRange.py
+++ scripts/Prod/binning/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-11-30 03:25:08 trottar"
+# Time-stamp: "2022-11-30 03:28:16 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -127,7 +127,7 @@ def find_tbins():
         # Progress bar
         Misc.progressBar(i, TBRANCH_RIGHT_DATA.GetEntries())
         H_t_Right.append(-evt.MandelT)
-    H_t_Right = np.histogram(H_t_Right,bins=200)
+    H_t_Right,rbins = np.histogram(H_t_Right,bins=200)
 
     H_t_Left = []
     print("\nCreating left t-bin histogram...")
@@ -136,7 +136,7 @@ def find_tbins():
         # Progress bar
         Misc.progressBar(i, TBRANCH_LEFT_DATA.GetEntries())
         H_t_Left.append(-evt.MandelT)
-    H_t_Left = np.histogram(H_t_Left,bins=200)
+    H_t_Left,lbins = np.histogram(H_t_Left,bins=200)
 
     H_t_Center = []
     print("\nCreating center t-bin histogram...")
@@ -145,7 +145,7 @@ def find_tbins():
         # Progress bar
         Misc.progressBar(i, TBRANCH_CENTER_DATA.GetEntries())
         H_t_Center.append(-evt.MandelT)
-    H_t_Center = np.histogram(H_t_Center,bins=200)
+    H_t_Center,cbins = np.histogram(H_t_Center,bins=200)
             
     for r,l,c in zip(H_t_Right,H_t_Left,H_t_Center):
 

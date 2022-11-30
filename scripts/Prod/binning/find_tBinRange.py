705f6b1296569be004066980b5016f21efdf7ef1
Richard Trotta <trotta@cua.edu>
2022-11-30 02:54:15 -0500

    Testing tbin script, phase 2

--- scripts/Prod/binning/find_tBinRange.py
+++ scripts/Prod/binning/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-11-30 02:53:06 trottar"
+# Time-stamp: "2022-11-30 02:54:12 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -82,7 +82,7 @@ def find_tbins():
     # Define root file trees of interest
 
     for val in ['Right', 'Left', 'Center']:
-        rootFiles = OUTPATH+"/"+InDATAFilename+"_%s.root" % val
+        rootFile = OUTPATH+"/"+InDATAFilename+"_%s.root" % val
         if not os.path.isfile(rootFile):
             continue
         else:

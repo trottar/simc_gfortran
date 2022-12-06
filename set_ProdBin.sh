609e3064fdb2472d806e8b2b86ecb2e336765a3c
Richard Trotta <trotta@cua.edu>
2022-12-06 00:44:32 -0500

    Adding random subtraction

--- set_ProdBin.sh
+++ set_ProdBin.sh
@@ -456,7 +456,7 @@ if [[ $t_flag = "true" || $d_flag = "true" ]]; then
     if [ ${#data_right[@]} -eq 0 ]; then
 	python3 find_tBinRange.py ${KIN} ${OutDATAFilename} ${OutFullAnalysisFilename} ${NumtBins} "${data_right[*]}" "${data_left[*]}" "${data_center[*]}" ${DataChargeSumRight} ${DataChargeSumLeft} ${DataChargeSumCenter} "${DataEffValRight[*]}" "${DataEffValLeft[*]}" "${DataEffValCenter[*]}" ${EffData}
     else
-	python3 find_tBinRange.py ${KIN} ${OutDATAFilename} ${OutFullAnalysisFilename} ${NumtBins} "0" "${data_left[*]}" "${data_center[*]}" "0" ${DataChargeSumLeft} ${DataChargeSumCenter} "0" "${DataEffValLeft[*]}" "${DataEffValCenter[*]}" ${EffData}
+	python3 find_tBinRange.py ${KIN} ${OutDATAFilename} ${OutFullAnalysisFilename} ${NumtBins} "0 " "${data_left[*]}" "${data_center[*]}" "0 " ${DataChargeSumLeft} ${DataChargeSumCenter} "0 " "${DataEffValLeft[*]}" "${DataEffValCenter[*]}" ${EffData}
     fi
 fi
 

df41fdce50ba22e02623e59dbc99d99e83cbcd98
Richard Trotta <trotta@cua.edu>
2022-12-17 16:56:21 -0500

    Testing simc changes

--- src/event.f
+++ src/event.f
@@ -1356,7 +1356,8 @@ C for Coulomb corrections, make sure the line below is NOT commented out.
 	if (debug(5)) write(6,*)'comp_rec_ev: Ein,E,uez=',recon%Ein,recon%e%E,recon%ue%z
 	recon%nu = recon%Ein - recon%e%E
 	recon%Q2 = 2*recon%Ein*recon%e%E*(1-recon%ue%z)
-	recon%q	= sqrt(recon%Q2 + recon%nu**2)
+!       recon%q	= sqrt(recon%Q2 + recon%nu**2)
+	recon%q	= -sqrt(recon%Q2)
 	recon%uq%x = - recon%e%P*recon%ue%x / recon%q
 	recon%uq%y = - recon%e%P*recon%ue%y / recon%q
 	recon%uq%z =(recon%Ein - recon%e%P*recon%ue%z)/ recon%q

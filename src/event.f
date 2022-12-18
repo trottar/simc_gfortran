ddcaa613786619aa987f278a661e794b350fbd53
Richard Trotta <trotta@cua.edu>
2022-12-18 18:40:04 -0500

    Testing simc changes

--- src/event.f
+++ src/event.f
@@ -513,11 +513,10 @@ C DJG spectrometer
 	  vertex%p%phi = atan2(vertex%up%y,vertex%up%x)
 	  if (vertex%p%phi.lt.0.) vertex%p%phi=vertex%p%phi+2.*pi
 	  write(6,*) 'vertex%p%xptar:',vertex%p%xptar
+	  write(6,*) 'vertex%p%P:',vertex%p%P
 	  call spectrometer_angles(spec%p%theta,spec%p%phi,vertex%p%xptar,vertex%p%yptar,vertex%p%theta,vertex%p%phi)
 	  call SetCentralAngles(vertex%p%theta,vertex%p%phi)
-!       call SetCentralAngles(spec%p%theta,spec%p%phi)
-!       call TransportToLab(vertex%p%P,vertex%up%x,vertex%up%y,vertex%up%z,vertex%p%xptar,vertex%p%yptar,rotmat)
-	  call TransportToLab(spec%p%P,vertex%up%x,vertex%up%y,vertex%up%z,vertex%p%xptar,vertex%p%yptar,rotmat)
+	  call TransportToLab(vertex%p%P,vertex%up%x,vertex%up%y,vertex%up%z,vertex%p%xptar,vertex%p%yptar,rotmat)
 	  vertex%p%E = sqrt(vertex%p%P**2+Mh2)
 	  vertex%p%delta = (vertex%p%P - spec%p%P)*100./spec%p%P
 	  if (debug(4)) write(6,*)'comp_ev: at 6'

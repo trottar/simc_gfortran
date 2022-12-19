3847bc52b994ef4d6eeb97fefa64c75388414849
Richard Trotta <trotta@cua.edu>
2022-12-19 18:40:44 -0500

    Testing simc changes

--- src/event.f
+++ src/event.f
@@ -1620,12 +1620,12 @@ CDJG Calculate the "Collins" (phi_pq+phi_targ) and "Sivers"(phi_pq-phi_targ) ang
 ! 	recon%Pmx = -recon%q*qx
 ! 	recon%Pmy = -recon%q*qy
 ! 	recon%Pmz = -recon%q*qz	
-!	recon%Pmx = fB(1) ! Lab
-!	recon%Pmy = fB(2) ! Lab
-!	recon%Pmz = fB(3) ! Lab
-	recon%Pmx = -bq(1) ! wrt q
-	recon%Pmy = -bq(2) ! wrt q
-	recon%Pmz = -bq(3) ! wrt q
+	recon%Pmx = fB(1) ! Lab
+	recon%Pmy = fB(2) ! Lab
+	recon%Pmz = fB(3) ! Lab
+!	recon%Pmx = -bq(1) ! wrt q
+!	recon%Pmy = -bq(2) ! wrt q
+!	recon%Pmz = -bq(3) ! wrt q
 	recon%Pm = sqrt(recon%Pmx**2+recon%Pmy**2+recon%Pmz**2)
 
 !STILL NEED SIGN FOR PmPer!!!!!!

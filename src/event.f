981a1f8f2f392f703b8ca8eeb3c3b022a9510e57
Richard Trotta <trotta@cua.edu>
2022-12-18 22:03:49 -0500

    Testing simc changes

--- src/event.f
+++ src/event.f
@@ -1949,13 +1949,13 @@ C If using Coulomb corrections, include focusing factor
 
 	dz = 1.0
 	
-!	pfx = upmag*upx0/sqrt(dx**2+dy**2+dz**2)
-!	pfy = upmag*upy0/sqrt(dx**2+dy**2+dz**2)
-!	pfz = upmag*upz0/sqrt(dx**2+dy**2+dz**2)
+	pfx = upmag*upx0/sqrt(dx**2+dy**2+dz**2)
+	pfy = upmag*upy0/sqrt(dx**2+dy**2+dz**2)
+	pfz = upmag*upz0/sqrt(dx**2+dy**2+dz**2)
 
-	pfx = upx0/sqrt(dx**2+dy**2+dz**2)
-	pfy = upy0/sqrt(dx**2+dy**2+dz**2)
-	pfz = upz0/sqrt(dx**2+dy**2+dz**2)	
+!	pfx = upx0/sqrt(dx**2+dy**2+dz**2)
+!	pfy = upy0/sqrt(dx**2+dy**2+dz**2)
+!	pfz = upz0/sqrt(dx**2+dy**2+dz**2)	
 
 	pf = [pfx,pfy,pfz]
 	v0 = [dx,dy,dz]
@@ -1973,9 +1973,9 @@ C If using Coulomb corrections, include focusing factor
 !	write(6,*) 'pfx:',pfx
 !	write(6,*) 'rotmat:',rotmat
 		
-!	write(6,*) 'before dx:',dx
+	write(6,*) 'before dx:',dx
 	dx = v(1)
-!	write(6,*) 'after dx:',dx
+	write(6,*) 'after dx:',dx
 	dy = v(2)
 	dz = v(3)
 	

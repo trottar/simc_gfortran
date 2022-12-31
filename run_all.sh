6965fd556c64f1ba76523423725f9a7e5b53ad6a
Richard Trotta <trotta@cua.edu>
2022-12-31 11:12:52 -0500

    lt_analysis section changes

--- run_all.sh
+++ run_all.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2022-12-31 10:51:47 trottar"
+# Time-stamp: "2022-12-31 11:12:46 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -11,30 +11,7 @@
 # Copyright (c) trottar
 #
 
-# Runs script in the ltsep python package that grabs current path enviroment
-if [[ ${HOSTNAME} = *"cdaq"* ]]; then
-    PATHFILE_INFO=`python3 /home/cdaq/pionLT-2021/hallc_replay_lt/UTIL_PION/bin/python/ltsep/scripts/getPathDict.py $PWD` # The output of this python script is just a comma separated string
-elif [[ ${HOSTNAME} = *"farm"* ]]; then
-    PATHFILE_INFO=`python3 /u/home/${USER}/.local/lib/python3.4/site-packages/ltsep/scripts/getPathDict.py $PWD` # The output of this python script is just a comma separated string
-fi
-
-# Split the string we get to individual variables, easier for printing and use later
-VOLATILEPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f1` # Cut the string on , delimitter, select field (f) 1, set variable to output of command
-ANALYSISPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f2`
-HCANAPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f3`
-REPLAYPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f4`
-UTILPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f5`
-PACKAGEPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f6`
-OUTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f7`
-ROOTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f8`
-REPORTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f9`
-CUTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f10`
-PARAMPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f11`
-SCRIPTPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f12`
-ANATYPE=`echo ${PATHFILE_INFO} | cut -d ','  -f13`
-USER=`echo ${PATHFILE_INFO} | cut -d ','  -f14`
-HOST=`echo ${PATHFILE_INFO} | cut -d ','  -f15`
-SIMCPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f16`
+SIMCPATH="/u/group/c-kaonlt/USERS/trottar/simc_gfortran"
 
 cd "${SIMCPATH}"
 

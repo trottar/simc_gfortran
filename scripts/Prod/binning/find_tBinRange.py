311af958e891378b14555e7370137126dfc80c94
Richard Trotta <trotta@cua.edu>
2023-01-12 16:50:55 -0500

    lt_analysis section changes

--- scripts/Prod/binning/find_tBinRange.py
+++ scripts/Prod/binning/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-12 16:46:35 trottar"
+# Time-stamp: "2023-01-12 16:50:50 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1015,7 +1015,7 @@ ptphithreek = TPaveText(0.419517,0.00514928,0.487128,0.0996315,"NDC")
 ptphithreek.AddText("#phi = #frac{3K}{2}")
 ptphithreek.Draw()
 Arc = TArc()
-for k in range(0, 10):
+for k in range(0, 7):
      Arc.SetFillStyle(0)
      Arc.SetLineWidth(2)
      # To change the arc radius we have to change number 0.825 in the lower line.
@@ -1024,7 +1024,7 @@ for i,(n,b) in enumerate(zip(binned_t0,binned_t[1])):
      Arc.SetLineColor(4)
      Arc.SetLineWidth(2)
      # To change the arc radius we have to change number 0.825 in the lower line.
-     Arc.DrawArc(0,0,b,0.,360.,"same")    
+     Arc.DrawArc(0,0,0.825*b(i+1)/10,0.,360.,"same")    
 tradius = TGaxis(0,0,0.575,0,tmin,tmax,10,"-+")
 tradius.SetLineColor(2)
 tradius.SetLabelColor(2)

ab6d4723e412fb19dd91a22dd703cd5beb75f147
Richard Trotta <trotta@cua.edu>
2023-01-12 13:40:00 -0500

    lt_analysis section changes

--- scripts/Prod/binning/find_tBinRange.py
+++ scripts/Prod/binning/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-12 13:38:24 trottar"
+# Time-stamp: "2023-01-12 13:39:56 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -753,7 +753,7 @@ binned_t0.append(binned_t[0][0])
 for n,b in zip(binned_t0,binned_t[1]):
     l_t.AddEntry(hist["H_t_DATA"],"Evts = %.0f" % n)
     l_t.AddEntry(hist["H_t_DATA"],"BinCenter = %.2f" % b)
-    tBin_line.SetLineColor(kBlack)
+    tBin_line.SetLineColor(7)
     tBin_line.SetLineWidth(2)
     tBin_line.DrawLine(b,0,b,binmax)
 

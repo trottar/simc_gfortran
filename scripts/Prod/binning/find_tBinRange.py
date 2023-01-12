7dcb9e0ccf4d44c32e042f05e77e98decda2c0a5
Richard Trotta <trotta@cua.edu>
2023-01-12 12:08:39 -0500

    lt_analysis section changes

--- scripts/Prod/binning/find_tBinRange.py
+++ scripts/Prod/binning/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-12 12:06:46 trottar"
+# Time-stamp: "2023-01-12 12:08:34 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -745,6 +745,7 @@ for i,hist in enumerate(histlist):
 
 tBin_line = TLine()
 print("¬¬¬¬¬¬¬¬¬¬¬¬¬",binned_t[1])
+binned_t[0].append(binned_t[0][0])
 print("¬¬¬¬¬¬¬¬¬¬¬¬¬",list(zip(binned_t[0],binned_t[1])))
 for n,b in zip(binned_t[0],binned_t[1]):
     print("¬¬¬¬¬¬¬¬¬¬¬¬",b)

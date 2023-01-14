94ed574f60eb67fd8b3dc0bbd0c2ddd87993e5a3
Richard Trotta <trotta@cua.edu>
2023-01-13 19:52:11 -0500

    lt_analysis section changes

--- scripts/Prod/binning/find_tBinRange.py
+++ scripts/Prod/binning/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-13 19:45:22 trottar"
+# Time-stamp: "2023-01-13 19:51:18 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1015,7 +1015,7 @@ ptphithreek = TPaveText(0.419517,0.00514928,0.487128,0.0996315,"NDC")
 ptphithreek.AddText("#phi = #frac{3K}{2}")
 ptphithreek.Draw()
 Arc = TArc()
-for k in range(0, 7):
+for k in range(0, 10):
      Arc.SetFillStyle(0)
      Arc.SetLineWidth(2)
      # To change the arc radius we have to change number 0.825 in the lower line.

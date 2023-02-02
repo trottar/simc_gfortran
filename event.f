6d24c69b3ee7a102fdc64e206e08b1bf064bcdfa
Richard Trotta <trotta@cua.edu>
2023-02-02 16:45:34 -0500

    Made changes Dave suggested

--- event.f
+++ event.f
@@ -1410,8 +1410,8 @@ c Everyone else in the world calculates W using the proton mass.
 !	write(6,*) 'spec%p%P:',spec%p%P
 !	write(6,*) '-recon%up%y:',-recon%up%y	
 	
-!	call SetCentralAngles(-spec%e%theta, 0.0, RotToLab)
-	write(6,*) 'e RotToLab%:',RotToLab
+	call SetCentralAngles(-spec%e%theta, 0.0, RotToLab)
+!	write(6,*) 'e RotToLab%:',RotToLab
 	call TransportToLab(recon%e%P, recon%e%xptar, recon%e%yptar, RotToLab, kf_vec)
 
 	fP = [0.0,0.0,ki,me]
@@ -1426,7 +1426,7 @@ c Everyone else in the world calculates W using the proton mass.
 	fA1 = fA+fQ
 	
 	call SetCentralAngles(spec%p%theta, 0.0, RotToLab)
-!	write(6,*) 'p RotToLab%:',RotToLab
+	write(6,*) 'p RotToLab%:',RotToLab
 	call TransportToLab(recon%p%P, recon%p%xptar, recon%p%yptar, RotToLab, Pf_vec)
 
 	write(6,*) 'Pf_vec:', Pf_vec

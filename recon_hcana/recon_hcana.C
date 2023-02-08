46e92c38a5fe59186a0c3f12e2f7b87433a4fb08
Richard Trotta <trotta@cua.edu>
2023-02-08 16:11:57 -0500

    Testing conversion script for simc variables to hcana calculations

---
+++ recon_hcana/recon_hcana.C
@@ -0,0 +1,55 @@
+/*
+ * Description:
+ * ================================================================
+ * Time-stamp: "2023-02-08 16:11:06 trottar"
+ * ================================================================
+ *
+ * Author:  Richard L. Trotta III <trotta@cua.edu>, Carlos Yero <cyero002@fiu.edu, cyero@jlab.org>
+ *
+ * Copyright (c) trottar
+ */
+
+#include<iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "recon_hcana.h"
+
+using namespace std;
+
+int main() {
+  string kinematics = "Q5p5W3p02_lowe";
+  string phi_setting = "Right";
+  vector<string> kinematics_split;
+  stringstream kinematics_stream(kinematics);
+  string kinematics_part;
+
+  while (getline(kinematics_stream, kinematics_part, '_')) {
+    kinematics_split.push_back(kinematics_part);
+  }
+
+  string InSIMCFilename = "OUTPUTS/Prod_Coin_" + kinematics_split[0] + phi_setting.lower() + "_" + kinematics_split[1];
+
+  string InSIMCHistname = InSIMCFilename + ".hist";
+  string InSIMCRootname = InSIMCFilename + ".root";
+  cout << "InSIMCFilename: " << InSIMCFilename << endl;
+  cout << "InSIMCRootname: " << InSIMCRootname << endl;
+
+  /*
+  TFile *f = new TFile(InSIMCRootname,"UPDATE");
+  TTree *tree = (TTree*)f->Get("h10");
+  Int_t x;
+  
+  tree->SetBranchAddress("x",&x);
+  
+  for (Int_t i=0;i<tree->GetEntries();i++) {
+    tree->GetEntry(i);
+    x=x*2;
+    tree->Fill();
+  }
+
+  tree->Write("",TObject::kOverwrite);
+  f->Close();
+  */
+  return 0;
+}

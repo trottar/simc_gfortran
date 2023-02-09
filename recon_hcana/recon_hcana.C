e789bfb2ae7630d7a4facd4fece0a266364feaed
Richard Trotta <trotta@cua.edu>
2023-02-08 21:58:48 -0500

    Testing conversion script for simc variables to hcana calculations

--- recon_hcana/recon_hcana.C
+++ recon_hcana/recon_hcana.C
@@ -1,7 +1,7 @@
 /*
  * Description:
  * ================================================================
- * Time-stamp: "2023-02-08 21:55:48 trottar"
+ * Time-stamp: "2023-02-08 21:58:44 trottar"
  * ================================================================
  *
  * Author:  Richard L. Trotta III <trotta@cua.edu>, Carlos Yero <cyero002@fiu.edu, cyero@jlab.org>
@@ -242,7 +242,6 @@ void recon_hcana::EventLoop(){
     Pmy = p_miss_q.Y();   //out-of-plane component (Oop)
 
     Pm = p_miss_q.Mag();
-    Em = p_miss_q.E();
     
     newTree->Fill();  
   }

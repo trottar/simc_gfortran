cd66b4d53996e957bdf2bca5fbef42ed38268c6b
Richard Trotta <trotta@cua.edu>
2023-02-08 21:07:17 -0500

    Testing conversion script for simc variables to hcana calculations

--- recon_hcana/recon_hcana.C
+++ recon_hcana/recon_hcana.C
@@ -1,7 +1,7 @@
 /*
  * Description:
  * ================================================================
- * Time-stamp: "2023-02-08 21:05:16 trottar"
+ * Time-stamp: "2023-02-08 21:07:14 trottar"
  * ================================================================
  *
  * Author:  Richard L. Trotta III <trotta@cua.edu>, Carlos Yero <cyero002@fiu.edu, cyero@jlab.org>
@@ -31,7 +31,7 @@ recon_hcana::recon_hcana() {
 
   simc_nevents = stod(split(FindString("Ngen",InSIMCHistname)[0], '=')[1]);
   simc_normfactor = stod(split(FindString("normfac",InSIMCHistname)[0], '=')[1]);
-  Ein = stod(num_split(split(FindString("Ebeam",InSIMCHistname)[0], '=')[1])[0]);
+  Ein = stod(split(FindString("Ebeam",InSIMCHistname)[0], '=')[1]);
   kf = stod(num_split(split(FindString("momentum",InSIMCHistname)[0], '=')[1])[0]);
   e_th = stod(num_split(split(FindString("angle",InSIMCHistname)[0], '=')[1])[0]);
   Pf = stod(num_split(split(FindString("momentum",InSIMCHistname)[0], '=')[1])[0]);

991f85354a0be890b2e86a912844a042659ee78c
Richard Trotta <trotta@cua.edu>
2023-02-08 22:06:53 -0500

    Testing conversion script for simc variables to hcana calculations

--- recon_hcana/recon_hcana.C
+++ recon_hcana/recon_hcana.C
@@ -1,7 +1,7 @@
 /*
  * Description:
  * ================================================================
- * Time-stamp: "2023-02-08 22:06:20 trottar"
+ * Time-stamp: "2023-02-08 22:06:48 trottar"
  * ================================================================
  *
  * Author:  Richard L. Trotta III <trotta@cua.edu>, Carlos Yero <cyero002@fiu.edu, cyero@jlab.org>
@@ -140,13 +140,6 @@ void recon_hcana::EventLoop(){
     tree->GetEntry(i);
 
     //--------Calculated Kinematic Varibales----------------
-
-    cout << "Ein: " << Ein << endl;
-    cout << "kf: " << kf << endl;
-    cout << "e_th: " << e_th << endl;
-    cout << "Pf: " << Pf << endl;
-    cout << "h_th: " << h_th << endl;  
-
     
     //Convert MeV to GeV
     Ein = Ein / 1000.;     //incident beam energy

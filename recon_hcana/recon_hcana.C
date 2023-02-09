cd3ad21fce8221ee4c05af537b2caae75c8f050a
Richard Trotta <trotta@cua.edu>
2023-02-09 18:38:44 -0500

    Testing conversion script for simc variables to hcana calculations

--- recon_hcana/recon_hcana.C
+++ recon_hcana/recon_hcana.C
@@ -1,7 +1,7 @@
 /*
  * Description:
  * ================================================================
- * Time-stamp: "2023-02-09 18:32:27 trottar"
+ * Time-stamp: "2023-02-09 18:38:02 trottar"
  * ================================================================
  *
  * Author:  Richard L. Trotta III <trotta@cua.edu>, Carlos Yero <cyero002@fiu.edu, cyero@jlab.org>
@@ -108,7 +108,7 @@ void recon_hcana::ReadTree(){
   tree->SetBranchAddress("sigcc", &sigcc);
   tree->SetBranchAddress("Weight", &Weight);  
   
-  newTree = tree->CloneTree(0);
+  newTree = tree->CloneTree();
 
   newTree->SetName("recon_hcana");
 
@@ -285,8 +285,8 @@ void recon_hcana::WriteHist(){
   cout << "Calling WriteHist() . . . " << endl;
   
   //tree->Write("",TObject::kOverwrite);
-  newTree->Write("recon_hcana",TObject::kOverwrite);
-  f->Delete("h10;*");
+  newTree->Write();
+  //f->Delete("h10;*");
   f->Close();
 
   cout << "Ending WriteHist() . . . " << endl;

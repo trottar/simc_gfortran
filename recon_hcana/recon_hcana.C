ed499b4f488440163a4cb030c02d68edb9b0a304
Richard Trotta <trotta@cua.edu>
2023-02-21 16:17:52 -0500

    Debugging with Dave

--- recon_hcana/recon_hcana.C
+++ recon_hcana/recon_hcana.C
@@ -1,7 +1,7 @@
 /*
  * Description:
  * ================================================================
- * Time-stamp: "2023-02-21 16:14:42 trottar"
+ * Time-stamp: "2023-02-21 16:17:45 trottar"
  * ================================================================
  *
  * Author:  Richard L. Trotta III <trotta@cua.edu>, Carlos Yero <cyero002@fiu.edu, cyero@jlab.org>
@@ -34,7 +34,6 @@ recon_hcana::recon_hcana() {
   simc_normfactor = stod(split(FindString("normfac",InSIMCHistname)[0], '=')[1]);
   Ein = stod(split(FindString("Ebeam",InSIMCHistname)[0], '=')[1]);
   k0 = num_split(split(FindString("momentum",InSIMCHistname)[0], '=')[1])[0];
-  kf = k0*(1+hsdelta/100);
   e_th = num_split(split(FindString("angle",InSIMCHistname)[0], '=')[1])[0];
   Pf = num_split(split(FindString("momentum",InSIMCHistname)[0], '=')[1])[1];
   h_th = num_split(split(FindString("angle",InSIMCHistname)[0], '=')[1])[1];
@@ -175,6 +174,8 @@ void recon_hcana::ReadTree(){
 void recon_hcana::EventLoop(){
 
   cout << "Calling EventLoop() . . . " << endl;
+
+  kf = k0*(1+hsdelta/100); // Corrected final electron momentum 
   
   //Convert MeV to GeV
   Ein = Ein / 1000.;     //incident beam energy

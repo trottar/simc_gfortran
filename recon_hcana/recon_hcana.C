cef06b67dc7b98e57d98d5d49a064af2bd810cd0
Richard Trotta <trotta@cua.edu>
2023-02-21 20:58:25 -0500

    Debugging with Dave

--- recon_hcana/recon_hcana.C
+++ recon_hcana/recon_hcana.C
@@ -1,7 +1,7 @@
 /*
  * Description:
  * ================================================================
- * Time-stamp: "2023-02-21 20:57:42 trottar"
+ * Time-stamp: "2023-02-21 20:58:19 trottar"
  * ================================================================
  *
  * Author:  Richard L. Trotta III <trotta@cua.edu>, Carlos Yero <cyero002@fiu.edu, cyero@jlab.org>
@@ -279,7 +279,7 @@ void recon_hcana::EventLoop(){
     Pmy_lab = fB.Y(); 
     Pmz_lab = fB.Z();
 
-    nu = (fP1.E() - fP0.E())
+    nu = (fP1.E() - fP0.E());
   
     Em = nu + fA.M() - fX.E();   
     

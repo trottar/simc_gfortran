d1ba03470b3305b8065a5886ec4a8d73549927fe
Richard Trotta <trotta@cua.edu>
2023-03-21 18:13:10 -0400

    Testing variable arguments

--- recon_hcana/recon_hcana.C
+++ recon_hcana/recon_hcana.C
@@ -1,7 +1,7 @@
 /*
  * Description:
  * ================================================================
- * Time-stamp: "2023-03-08 20:35:11 trottar"
+ * Time-stamp: "2023-03-21 18:12:24 trottar"
  * ================================================================
  *
  * Author:  Richard L. Trotta III <trotta@cua.edu>, Carlos Yero <cyero002@fiu.edu, cyero@jlab.org>
@@ -18,8 +18,10 @@
 
 using namespace std;
 
-recon_hcana::recon_hcana() {
+recon_hcana::recon_hcana(arg1) {
 
+  cout << arg1 << endl;
+  
   buildFileName();
 
   InSIMCHistname = InSIMCFilename + ".hist";

b4c828697441f71361e48b218a512c37b5c29175
Richard Trotta <trotta@cua.edu>
2024-02-13 00:02:27 -0500

    Testing aero/hgcer geometric cuts in recon_hcana

--- recon_hcana/recon_hcana.C
+++ recon_hcana/recon_hcana.C
@@ -1,7 +1,7 @@
 /*
  * Description:
  * ================================================================
- * Time-stamp: "2024-02-12 23:53:42 trottar"
+ * Time-stamp: "2024-02-13 00:02:24 trottar"
  * ================================================================
  *
  * Author:  Richard L. Trotta III <trotta@cua.edu>, Carlos Yero <cyero002@fiu.edu, cyero@jlab.org>
@@ -589,7 +589,7 @@ void recon_hcana::EventLoop(){
     cutg->SetPoint(19, -25, 0.5);
     cutg->SetPoint(20, -25, 2);
 
-    if ((cutg->IsInside(phgcer_y_det, phgcer_x_det)) || (paero_tray_cut)){
+    if (!(cutg->IsInside(phgcer_y_det, phgcer_x_det)) || !(paero_tray_cut)){
       cout << "Event outside geometric acceptance..." << endl;
       continue;
     }

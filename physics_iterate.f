44246ddfaa05ef0d4f6958b0a929f72d9f2fd27c
Richard Trotta <trotta@cua.edu>
2025-01-08 13:31:24 -0500

    Best ratio for Q2=3.0,W=3.14 Center only

--- physics_iterate.f
+++ physics_iterate.f
@@ -38,7 +38,7 @@ c	real*8 efer			!energy of target particle
 
 *       RLT (7/11/2024): Redefined functional forms of L, T, LT, TT
 *       that incorporates Q2-dep based of pi FF
-	real Qdep_L, Qdep_T
+	real Qdep_L, Qdep_T, Qdep_LT, Qdep_TT
 	
 	real*8 s,s_gev,t,t_gev,Q2_g			!t,s, Q2 in (GeV/c)**2
 	real*8 mtar_gev
@@ -398,73 +398,23 @@ c	write(6,*)' phicm ',phicm*180./3.14159,phicm_fer*180./3.14159,phipq*180./3.141
 *       RLT (10/8/2023): Testing new tav parameterization
 	   tav=(0.1112 + 0.0066*log(q2_set))*q2_set
 	   ftav=(abs(t_gev)-tav)/tav
-*       ft=t_gev/(abs(t_gev)+mkpl**2)**2
-*       RLT (9/21/2023): t_gev should be abs(t_gev)
-	   ft=abs(t_gev)/(abs(t_gev)+mkpl**2)**2
-
-*	   sigl=(fitpar(1)+fitpar(2)*log(Q2_g))
-*       1           *exp((fitpar(3)+fitpar(4)*log(Q2_g))*(abs(t_gev)-0.2))
-*       RLT (10/12/2023): Removed 0.2 to keep things as simple as possible for
-*       initial start parameterization
-*       RLT (2/19/2024): Adding a 0.2 term to t dependence to bring down the
-*                        extreme slope at high t
-*       RLT (3/09/2024): Removing +0.2 term for better parameterization of
-*                        Q2=3.0, W=2.32	   
-**	   sigl=(fitpar(1)+fitpar(2)*log(Q2_g))
-**     1           *exp((fitpar(3)+fitpar(4)*log(Q2_g))*(abs(t_gev)))
-*       sigl=(fitpar(1)+fitpar(2)*log(Q2_g))
-*       1           *exp((fitpar(3)+fitpar(4)*log(Q2_g))*(abs(t_gev)+0.2))
-*     RLT (6/04/2024): Testing simplier exp form for L+T
-**
-**	   sigl=(fitpar(1)+fitpar(2)*log(Q2_g))
-**     1           *exp(fitpar(3)*(abs(t_gev)))
-*       RLT (2/15/2024): Removing t dependence from sigT because it seems
-*                        to be driving poor sep xsects results
-*       RLT (2/20/2024): Added 1/Q^4 term to dampen sigT
-*       RLT (2/21/2024): Using global analysis sig T model and params
-*       (https://journals.aps.org/prc/pdf/10.1103/PhysRevC.85.018202)
-**	   sigt=fitpar(5)+fitpar(6)*log(Q2_g)
-**     1           +(fitpar(7)+fitpar(8)*log(Q2_g))*ftav
-*       sigt=fitpar(5)+fitpar(6)*log(Q2_g)
-*       sigt=fitpar(5)*log(Q2_g)+fitpar(6)/(Q2_g**2)
-*	   sigt=fitpar(5)/(1+fitpar(6)*Q2_g)
-*     RLT (6/04/2024): Testing simplier exp form for L+T
-**
-**	   sigt=(fitpar(5)*((abs(t_gev)/Q2_g)-1))
-**     1           *exp(fitpar(6)*(abs(t_gev)))
-
-	   siglt=(fitpar(9)*exp(fitpar(10)*abs(t_gev))
-     1           +fitpar(11)/abs(t_gev))*sin(thetacm)
-*       RLT (1/2/2024): Need to have 16 parameters (4 for L/T/LT/TT) for
-*                       the xfit_in_t.py script to work. LT/TT are zeros
-*                       Therefore param 12 was also changed to 13      	   
-	   sigtt=(fitpar(13)*Q2_g*exp(-Q2_g))*ft*sin(thetacm)**2
-
-*       RLT (9/25/2023): There are two tav parameterizations in here.
-*                        I am only using the one above, for now.	   
-*	   tav=(-0.178+0.315*log(Q2_g))*Q2_g	   
-
-**      
-*     RLT (7/11/2024): Redefined functional forms of L, T, LT, TT
-*                      that incorporates Q2-dep based of pi FF
 	   ft=abs(t_gev)/(abs(t_gev)+mkpl**2)**2 ! pole factor
+	   
 	   Qdep_L=Q2_g/(1.0+(1.77*Q2_g)+0.12*(Q2_g**2))
-**      sigL=(fitpar(1)*Qdep_L*ft)*exp(-fitpar(2)*(abs(t_gev)))
-	   sigL=(fitpar(1)*Qdep_L*ft)*exp(-fitpar(2)*(abs(t_gev)))
-*     sigT=(fitpar(5)/Q2_g)*exp(-fitpar(6)*(Q2_g**2))
 	   Qdep_T=(exp(-Q2_g**2))/Q2_g
-**      sigT=fitpar(5)*(fitpar(6)+exp(-fitpar(7)*(abs(t_gev))))*(Qdep_T**fitpar(8))
+	   Qdep_TT=Q2_g*(exp(-Q2_g))
+	   
+	   sigL=(fitpar(1)*Qdep_L*ft)*exp(-fitpar(2)*(abs(t_gev)))
+
 	   sigT=(fitpar(5)*exp(-fitpar(6)*(abs(t_gev)))+fitpar(7)*(abs(t_gev)))
      >          *(Qdep_T**fitpar(8))
-***      sigLT=(fitpar(9)/(1+Q2_g))*sin(thetacm)
-***     >     *exp(-fitpar(10)*(abs(t_gev)))
-*      sigLT=(fitpar(9)*exp(fitpar(10)*abs(t_gev))
-*     >     +fitpar(11)/abs(t_gev))*sin(thetacm)
-*      sigTT=(-fitpar(13)/(1+Q2_g))*(sin(thetacm)**2)
-*     >     *exp(-fitpar(14)*(abs(t_gev)))
-***      sigTT=(fitpar(13)/(1+Q2_g))*(sin(thetacm)**2)
-***     >     *ft*exp(-fitpar(14)*(Q2_g))
-
+	   
+	   sigLT=(fitpar(9)*exp(-fitpar(10)*(abs(t_gev)))+fitpar(11)/(abs(t_gev)))
+     >          *sin(thetacm)
+	   
+	   sigTT=fitpar(13)+fitpar(14)*(exp(-fitpar(15)*(abs(t_gev)**2))
+     >          /(1.0+abs(t_gev))**(fitpar(16)))
+     >          *Qdep_TT*(sin(thetacm)**2)
 	   
 	   sig219=(sigt+main%epsilon*sigl+main%epsilon*cos(2.*phicm)*sigtt
      >		+sqrt(2.0*main%epsilon*(1.+main%epsilon))*cos(phicm)*siglt)/1.d0

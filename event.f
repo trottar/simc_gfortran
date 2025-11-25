72833b754306793105d5bea2bcb06733e98f9b82
trottar <trottar@jlab.org>
2025-11-25 02:59:37 -0500

    Added bash script

--- event.f
+++ event.f
@@ -1358,16 +1358,25 @@ CDJG Calculate the "Collins" (phi_pq+phi_targ) and "Sivers"(phi_pq-phi_targ) ang
       subroutine complete_main(force_sigcc,main,vertex,vertex0,recon,success)
 
       USE structureModule
+      USE F1F2IN21_MOD
       implicit none
       include 'simulate.inc'
 
-      integer         i, iPm1
-      real*8          a, b, r, frac, peepi, peeK, peedelta, peerho, peepiX
-      real*8          survivalprob, semi_dilution
-      real*8          weight, width, sigep, deForest, tgtweight
-      logical         force_sigcc, success
-      type(event_main):: main
-      type(event)::    vertex, vertex0, recon
+      integer      i, iPm1
+      real*8       a, b, r, frac, peepi, peeK, peedelta, peerho, peepiX
+      real*8       survivalprob, semi_dilution
+      real*8       weight, width, sigep, deForest, tgtweight
+
+C--- New variables for F1F2 heavy-target inclusive cross section
+      real*8       alpha_em, Mp_g
+      real*8       F1_v, F2_v, F1_r, F2_r
+      real*8       Q2g_v, W2g_v, Q2g_r, W2g_r
+      real*8       Ei_g, Ef_g, nu_g, theta_e
+      real*8       sin2, cos2, sigma_mott, W1, W2
+
+      logical      force_sigcc, success
+      type(event_main) :: main
+      type(event)      :: vertex, vertex0, recon
 	
 !-----------------------------------------------------------------------
 ! Calculate everything left in the /main/ structure that hasn't been
